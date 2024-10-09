9541f99a8b7393bdb0c186b610bd45778bb3d5e8
diekred <leonardo.diek11@gmail.com>
2024-10-09 12:05:57 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    question:"De que forma as mudanças climáticas impactam diretamente a vida de crianças e adolescentes, e por que esses grupos são mais vulneráveis?",
+    answers: [
+      
+      { option: "A) As mudanças climáticas não impactam diretamente crianças e adolescentes, pois os efeitos do clima afetam apenas adultos.", correct: false },
+      { option: "B) Crianças e adolescentes são mais vulneráveis às mudanças climáticas porque têm sistemas imunológicos mais fortes, capazes de resistir aos impactos ambientais.", correct: false },
+      { option: "C) As mudanças climáticas afetam diretamente crianças e adolescentes devido à sua fisiologia e desenvolvimento em fase sensível, tornando-os mais suscetíveis a problemas de saúde, desnutrição e dificuldades em acessar serviços básicos como água potável e educação.", correct: true },
+      { option: "D) Crianças e adolescentes são menos vulneráveis às mudanças climáticas porque vivem em áreas urbanas com infraestrutura avançada, protegendo-os dos efeitos climáticos extremos.", correct: false },
+      { option: "E) As mudanças climáticas impactam crianças e adolescentes de forma igual a qualquer outro grupo etário, sem causar efeitos mais graves sobre sua saúde ou bem-estar.", correct: false },
+      
+    ],
+  },
+    {
+      question: "Quais são as principais ações que crianças e adolescentes podem tomar dentro de suas escolas para promover um ambiente mais sustentável?",
+      answers: [
+        { option: "A) Crianças e adolescentes podem promover um ambiente mais sustentável na escola apenas estudando os impactos ambientais sem realizar ações práticas.", correct: false },
+        { option: "B) Para promover a sustentabilidade na escola, é importante que crianças e adolescentes evitem o uso de qualquer tipo de tecnologia, pois todas as tecnologias prejudicam o meio ambiente.", correct: false },
+        { option: "C) Uma forma eficaz de promover a sustentabilidade é substituir todas as atividades acadêmicas por atividades esportivas, eliminando completamente o uso de materiais escolares.", correct: false },
+        { option: "D) Crianças e adolescentes podem contribuir reciclando materiais, economizando água e energia, promovendo campanhas de conscientização ambiental e criando hortas escolares.", correct: true },
+        { option: "E) Para promover um ambiente sustentável, é necessário que crianças e adolescentes troquem todos os materiais recicláveis por materiais novos e descartáveis para facilitar o descarte.", correct: false },
+      ],
+    },
+    {
+      question: "Qual é a importância da educação ambiental no contexto das mudanças climáticas, e como isso pode ser implementado nas escolas brasileiras?",
+      answers: [
+        { option: "A) A educação ambiental é essencial para sensibilizar e preparar crianças e adolescentes sobre os impactos das mudanças climáticas, promovendo ações práticas como reciclagem, economia de recursos e projetos ambientais nas escolas.", correct: true },
+        { option: "B) A educação ambiental não tem muita relevância no contexto das mudanças climáticas, já que a conscientização sobre o meio ambiente é mais eficaz em outros espaços, como redes sociais.", correct: false },
+        { option: "C) A educação ambiental é importante porque ensina as crianças a evitarem totalmente o contato com a natureza, para não prejudicar o meio ambiente.", correct: false },
+        { option: "D) A educação ambiental deve ser implementada nas escolas apenas através de aulas teóricas sobre o meio ambiente, sem necessidade de práticas sustentáveis no cotidiano escolar.", correct: false },
+        { option: "E) A educação ambiental pode ser implementada nas escolas brasileiras promovendo o uso de materiais descartáveis, já que a reciclagem é difícil de aplicar no ambiente escolar.", correct: false },
+      ],
+    },
+    {
+      question: "Como o consumo energético no Brasil influencia nas emissões de CO2, e como a transição para energias renováveis poderia ser acelerada?",
+      answers: [
+        { option: "A) O consumo energético no Brasil não influencia as emissões de CO2, pois a maioria da energia utilizada é proveniente de fontes não renováveis, que não geram emissões.", correct: false },
+        { option: "B) O consumo energético no Brasil contribui para as emissões de CO2 principalmente por causa do desmatamento, e a transição para energias renováveis pode ser acelerada ignorando as políticas ambientais existentes.", correct: false },
+        { option: "C) O consumo energético no Brasil influencia as emissões de CO2, uma vez que cerca de 48,4% do consumo vem de fontes fósseis, como petróleo e gás natural; a transição para energias renováveis pode ser acelerada através de investimentos em tecnologias limpas e políticas públicas que incentivem o uso de fontes como solar e eólica.", correct: true },
+        { option: "D) As emissões de CO2 no Brasil são irrelevantes, pois o país possui uma matriz energética totalmente limpa, tornando desnecessária qualquer transição para energias renováveis.", correct: false },
+        { option: "E) O consumo energético no Brasil é uma questão secundária nas emissões de CO2, e a transição para energias renováveis deve focar apenas em melhorar a eficiência energética sem mudar as fontes de energia.", correct: false },
+      ],
+    },
+    {
+      question: "Quais são as implicações das desigualdades sociais no Brasil em relação à vulnerabilidade de crianças e adolescentes frente às mudanças climáticas?",
+      answers: [
+        { option: "A) As desigualdades sociais no Brasil não têm influência significativa sobre a vulnerabilidade de crianças e adolescentes em relação às mudanças climáticas, já que os impactos climáticos afetam todas as pessoas igualmente.", correct: false },
+        { option: "B) A vulnerabilidade de crianças e adolescentes às mudanças climáticas é baixa no Brasil, independentemente das desigualdades sociais, pois a maioria das áreas afetadas por desastres está bem equipada com serviços de proteção e assistência", correct: false },
+        { option: "C) As desigualdades sociais no Brasil não estão relacionadas às mudanças climáticas, já que os impactos do clima são determinados principalmente pela localização geográfica, e não pelas condições socioeconômicas.", correct: false },
+        { option: "D) As desigualdades sociais tornam crianças e adolescentes mais vulneráveis às mudanças climáticas, pois os mais pobres têm menos acesso a serviços essenciais, como saúde, educação e saneamento, o que os deixa expostos a riscos maiores de desastres ambientais e crises de saúde.", correct: true },
+      ],
+    },
+  ];
